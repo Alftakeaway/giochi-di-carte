6ab39c7131a1d623400307c9107fd3b621c7531f
Alftakeaway <93295977+Alftakeaway@users.noreply.github.com>
2026-08-20 13:30:21 +0200

    Napoletane: facce dalle scansioni singole ad alta risoluzione di Wikimedia (500-1324px, non più la griglia a bassa risoluzione), centrate su ratio 1.42 e angoli smussati; JPG ottimizzati

--- js/ui.js
+++ js/ui.js
@@ -40,7 +40,7 @@ const UI = {
     // (es. img/napoletane/denari_07.jpg) al posto del rendering CSS/SVG.
     const usaImmagine = !carta.jolly && !!confMazzo.immagini;
     const percorsoImmagine = usaImmagine
-      ? `img/${carta.tipoMazzo}/${carta.seme.nome}_${String(carta.valore).padStart(2, '0')}.png`
+      ? `img/${carta.tipoMazzo}/${carta.seme.nome}_${String(carta.valore).padStart(2, '0')}.jpg`
       : '';
 
     node.innerHTML = `
